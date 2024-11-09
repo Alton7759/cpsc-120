dbb1ac9308c2ff8c489fd7164106ddb4fc329684
Alton7759 <112207172+Alton7759@users.noreply.github.com>
2024-11-09 14:10:36 -0800

    lab 3

---
+++ lab 03/part-1/sandwich.cc
@@ -0,0 +1,33 @@
+// Christian Alton Bonilla
+// CPSC 120-01
+// 2022-08-21
+// Alton7@csu.fullerton.edu
+// @Alton7759
+//
+// Lab 03-01
+// Partners: @annavera38
+//
+// you make a order for a sandwitch!
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  string portein;
+  cout << "Enter protein: ";
+  cin >> portein;
+  string bread;
+  cout << "Enter bread: ";
+  cin >> bread;
+  string condiment;
+  cout << "Enrer condiment: ";
+  cin >> condiment;
+  cout << "Your order:\n"
+       << "A " << portein << " sandwich on " << bread << " with " << condiment
+       << "\n";
+
+  return 0;
+}
---
+++ lab 03/part-2/units.cc
@@ -0,0 +1,30 @@
+// Christian Alton Bonilla
+// CPSC 120-01
+// 2022-08-21
+// Alton7@csu.fullerton.edu
+// @Alton7759
+//
+// Lab 03-01
+// Partners: @annavera38
+//
+// Convert milliliters to units of the US customary system.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  double milliliters{69};
+  cout << "Ender Ml: ";
+  cin >> milliliters;
+  double teaspoon{milliliters / 4.929};
+  double tablespoon{teaspoon / 3};
+  double ounce{tablespoon / 2};
+  double cup{ounce / 8};
+  cout << milliliters << " ml = " << teaspoon << " tsp = " << tablespoon
+       << " tbsp = " << ounce << " oz = " << cup << " cups\n";
+
+  return 0;
+}
